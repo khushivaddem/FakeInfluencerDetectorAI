429a65b46c9c72dcba2890d98f9fd49e166989c2
Khushi Vaddem <vaddemkhushi@gmail.com>
2026-08-07 20:30:14 +0530

    Updated API URL

--- frontend/script.js
+++ frontend/script.js
@@ -9,7 +9,7 @@ document.getElementById("analyzeBtn").addEventListener("click", async () => {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://fake-influencer-api.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
